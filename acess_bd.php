32e366652871a0528b85a2f68b50cc9f15f8a6f6
mariaspcc <43615324+mariaspcc@users.noreply.github.com>
2019-11-22 22:30:20 +0000

    MINHA PASS MUDADA PARA MINUSCULAS

--- acess_bd.php
+++ acess_bd.php
@@ -1,6 +1,6 @@
 <?php
 
-$str = "dbname=postgres user=postgres password=POSTGRES host=localhost port=5432";
+$str = "dbname=postgres user=postgres password=postgres host=localhost port=5432";
 $connection = pg_connect($str);
 if (!$connection) {
     die("Erro na ligacao"); }
