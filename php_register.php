56936ab17cbc6109b4fcb345ef0f559bd6920e94
mariaspcc <43615324+mariaspcc@users.noreply.github.com>
2019-11-24 11:16:33 +0000

    cliente

--- php_register.php
+++ php_register.php
@@ -8,12 +8,12 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 
 //verifica se ursename existe na BD
-$query1 = "SELECT * FROM utilizador_administrador_cliente WHERE ('$username' = username)";
+$query1 = "SELECT * FROM cliente WHERE ('$username' = user_username)";
 $result1 = pg_query($connection, $query1);
 
 //caso não exista, insere registo na BD
 if (pg_num_rows($result1)==0) {
-    $query = "INSERT INTO utilizador_administrador_cliente (Username, Password) VALUES ('$username', '$password')";
+    $query = "INSERT INTO cliente (user_username, user_password) VALUES ('$username', '$password')";
     $result = pg_query($connection, $query);
 }
 
