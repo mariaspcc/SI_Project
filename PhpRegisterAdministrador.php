3cc66fac6b304f00ede6a1e96e2fa78ea88b0087
Maria Figueira <34713532+mariafigueira@users.noreply.github.com>
2019-11-25 17:53:55 +0000

    ...

---
+++ PhpRegisterAdministrador.php
@@ -0,0 +1,29 @@
+<?php
+
+echo "entra";
+//chama acess_bd.php para ligar à BD
+include_once "acess_bd.php";
+
+echo "depois";
+//buscar dados
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+
+$query1 = "SELECT * FROM administrador WHERE ('$username' = user_username)";
+$query2= "SELECT * FROM cliente WHERE ('$username' = user_username)";
+
+$result1 = pg_query($connection, $query1);
+$result2 = pg_query($connection, $query2);
+
+//caso não exista, insere registo na BD
+if(pg_num_rows($result1) == 0 && pg_num_rows($result2) == 0) {
+    $query = "INSERT INTO administrador (user_username, user_password) VALUES ('$username', '$password')";
+
+    $result = pg_query($connection, $query);
+}
+
+//fecha ligação à BD
+pg_close($connection);
+
+?>
--- PhpRegisterCliente.php
+++ PhpRegisterCliente.php
@@ -9,25 +9,17 @@ echo "depois";
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-$query1="";
 
-//verifica se ursename existe na BD
-if(isset($_POST['B_cliente'])) {
-$query1 = "SELECT * FROM cliente WHERE ('$username' = user_username)";
-}
-if(isset($_POST['B_admin'])) {
 $query1 = "SELECT * FROM administrador WHERE ('$username' = user_username)";
-}
+$query2= "SELECT * FROM cliente WHERE ('$username' = user_username)";
+
 $result1 = pg_query($connection, $query1);
+$result2 = pg_query($connection, $query2);
 
 //caso não exista, insere registo na BD
-if(pg_num_rows($result1) == 0) {
-    if(isset($_POST['B_cliente'])) {
+if(pg_num_rows($result1) == 0 && pg_num_rows($result2) == 0) {
     $query = "INSERT INTO cliente (user_username, user_password) VALUES ('$username', '$password')";
-    }
-    if(isset($_POST['B_admin'])) {
-    $query = "INSERT INTO administrador (user_username, user_password) VALUES ('$username', '$password')";
-    }
+
     $result = pg_query($connection, $query);
 }
 
