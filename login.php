3d179faa03fea0d60872fbf765ca26545bf5a59f
mariaspcc <43615324+mariaspcc@users.noreply.github.com>
2019-11-25 23:48:26 +0000

    LOGIN MUITO MELHOR

--- login.php
+++ login.php
@@ -6,28 +6,22 @@ include_once "acess_bd.php";
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-//$username = pg_real_escape_string($_POST['username']);
-//$password = pg_real_escape_string($_POST['password']);
-
-//$result=pg_query("Select * from cliente, administrador where user_username = '$username' and user_password = '$password'")
-//or die("Falha");
-
-
 $query1 = "SELECT * FROM administrador WHERE ('$username' = user_username)";
-//$query2 = "SELECT * FROM cliente WHERE ('$username' = user_username)";
+$query2 = "SELECT * FROM cliente WHERE ('$username' = user_username)";
 
 $result1 = pg_query($connection, $query1);
-//$result2 = pg_query($connection, $query2);
+$result2 = pg_query($connection, $query2);
 
-$row = pg_fetch_array($result1);
+$row1 = pg_fetch_array($result1);
+$row2 = pg_fetch_array($result2);
 
-if($row['username'] == $username && $row['password'] == $password){
+if($row1['user_username'] == $username && $row1['user_password'] == $password || $row2['user_username'] == $username && $row2['user_password'] == $password){
     echo "LOGADO!";
 }
 else {
     echo "NOT LOGADO!";
 }
 
-//pg_close($connection);
+pg_close($connection);
 
 ?>
