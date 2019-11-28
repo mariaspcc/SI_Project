e00115190b88b06091d65302b938f1bf5fa5e7a5
mariaspcc <43615324+mariaspcc@users.noreply.github.com>
2019-11-28 14:54:08 +0000

    outro update

--- CriarPrato.php
+++ CriarPrato.php
@@ -18,7 +18,7 @@ if (!isLoggedIn()) {
 <html lang="pt">
 <head>
     <meta charset="UTF-8">
-    <title>Homepage_cliente</title>
+    <title>CriarPrato</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="style.css">
 </head>
@@ -28,9 +28,25 @@ if (!isLoggedIn()) {
 <div class="fundo_quadrado"></div>
 <?php include('header_in.php'); ?>
 <main>
-
-
+    <div class="FormPrato">
+        <p> CRIAR PRATO </p>
+        <form action="CriarPrato.php" method="POST" id="form_prato">
+            <label> <br>Nome do Prato
+                    <input type="text" name="nome_prato" required>
+            </label>
+            <br>
+            <label> <br>Preço
+                <input type="number" name="prato_preço" required></label>
+            <br>
+            <label> <br>Detalhes
+                <input type="text" name="detalhes_prato" required>
+            </label>
+            <br>
+            <input type="submit" name="register" value="Guardar">
+        </form>
+    </div>
 </main>
 </body>
 </html>
+
 
