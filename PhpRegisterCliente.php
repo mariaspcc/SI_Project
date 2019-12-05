e83866035fa26af1f83a17792e6cfcde635772e5
Maria Figueira <34713532+mariafigueira@users.noreply.github.com>
2019-12-05 15:47:19 +0000

    olá!
    lol

--- PhpRegisterCliente.php
+++ PhpRegisterCliente.php
@@ -7,20 +7,20 @@ if(isset($_POST['register'])) {
     $password = $_POST['c_password'];
 
 
-    $query1 = "SELECT * FROM administrador WHERE ('$username' = user_username)";
-    $query2 = "SELECT * FROM cliente WHERE ('$username' = user_username)";
 
+    $query1="SELECT * FROM usergeral WHERE ('$username' = username)";
+    $result1 = pg_query($connection, $query1);
     $name_error = "O username já existe.";
 
-    $result1 = pg_query($connection, $query1);
-    $result2 = pg_query($connection, $query2);
 
     //caso não exista, insere registo na BD
-    if (pg_num_rows($result1) > 0 || pg_num_rows($result2) > 0) {
+    if (pg_num_rows($result1) > 0) {
         $name_error = "O username já existe.";
     } else {
-        $query = "INSERT INTO cliente (user_username, user_password) VALUES ('$username', '$password')";
+        $query = "INSERT INTO usergeral (username, password) VALUES ('$username', '$password')";
         $result = pg_query($connection, $query);
+        $query2 = "INSERT INTO cliente (usergeral_username) VALUES ('$username')";
+        $result2 = pg_query($connection, $query2);
         echo "SAVED";
         //exit();
         header('location: Homepage_Cliente.php');
