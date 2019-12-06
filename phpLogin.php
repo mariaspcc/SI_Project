a08b15223e7546f91418cf648ff010616271b5cd
Maria Figueira <34713532+mariafigueira@users.noreply.github.com>
2019-12-06 20:07:58 +0000

    ESTOU PASSADA

--- phpLogin.php
+++ phpLogin.php
@@ -8,21 +8,29 @@ if(isset($_POST['login'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-  /*  $query1 = "SELECT * FROM administrador WHERE '$username' = usergeral_username AND '$password'= usergeral_password";
-    $query2 = "SELECT * FROM cliente WHERE '$username' = usergeral_username AND '$password'= usergeral_password";*/
+    /*  $query1 = "SELECT * FROM administrador WHERE '$username' = usergeral_username AND '$password'= usergeral_password";
+      $query2 = "SELECT * FROM cliente WHERE '$username' = usergeral_username AND '$password'= usergeral_password";*/
     $query1 = "SELECT * FROM usergeral WHERE '$username' = username AND '$password'= password";
+    $tipo = "SELECT administrador FROM usergeral WHERE '$username' = username AND '$password'= password";
     $result1 = pg_query($connection, $query1);
+    $result2 = pg_query($connection, $tipo);
+    $a=pg_fetch_array($result2);
 
-    /*$result1 = pg_query($connection, $query1);
-    $result2 = pg_query($connection, $query2);
-*/
     $name_error = "Username ou password incorretos.";
 
-    if(pg_num_rows($result1)==1 ) {
+    if (pg_affected_rows($result1) == 1 && $a['administrador'] == "true") {
         $_SESSION['username'] = $username;
         $_SESSION['sucess'] = "Entrou!";
         header('location: Homepage_Administrador.php');
-
+    } else if (pg_affected_rows($result1) == 1 &&  $a['administrador']== "false") {
+        $_SESSION['username'] = $username;
+        $_SESSION['sucess'] = "Entrou!";
+        header('location: Homepage_Cliente.php');
+    }
+    else if(pg_affected_rows($result1)==0){
+        $name_error = "Username ou password incorretos.";
+    }
+}
 
         /*
             if(pg_num_rows($result1)==1 && pg_num_rows($result2)==0){
@@ -42,7 +50,7 @@ if(isset($_POST['login'])) {
             }
         */
 
-    }}
+
 
 //FUNCIONA
 function isLoggedIn()
