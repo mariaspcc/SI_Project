293bb9b91a088e7cebe630f904e144eee2e66edf
Maria Figueira <34713532+mariafigueira@users.noreply.github.com>
2019-12-08 18:18:47 +0000

    GRRRRRRR

--- CriarPrato.php
+++ CriarPrato.php
@@ -1,7 +1,27 @@
 <?php
+session_start();
+include_once "acess_bd.php";
+if (isset($_POST['register'])) {
+    $comprado = "false";
+    $nome_prato = $_POST['nome_prato'];
+    $tipo_prato = $_POST['tipo_prato'];
+    $preco = $_POST['preco_prato'];
+    $descricao = $_POST['descricao_prato'];
+    $restaurante_nome = $_POST['restaurante'];
+    $administrador_usergeral_username = $_SESSION['username'];
 
-include('phpCriarPrato.php');
-
+    $query1 = "SELECT * FROM prato WHERE ('$nome_prato' = nome_prato)";
+    $result1 = pg_query($connection, $query1);
+    if (pg_affected_rows($result1) > 0) {
+        $name_error = "Já foi criado um prato com esse nome";
+    } else {
+        $query = "INSERT INTO prato (nome,tipo,descricao,preco,comprado,restaurante_nome,administrador_usergeral_username) VALUES ('$nome_prato','$tipo_prato','$descricao','$preco','$comprado','$restaurante_nome','$administrador_usergeral_username')";
+        $result1 = pg_query($connection, $query);
+        //echo "SAVED";
+        //exit();
+        header('location: Homepage_Administrador.php');
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -18,13 +38,31 @@ include('phpCriarPrato.php');
 <div class="fundo_quadrado"></div>
 <?php include('header_in.php'); ?>
 <main>
-        <p> CRIAR PRATO </p>
-    <?php if(isset($name_error)){?>
-        <span><?php echo $name_error;?></span>
-    <?php }?>
+    <p> CRIAR PRATO </p>
+    <?php if (isset($_SESSION['success']) && ($_SESSION['success'])) { ?>
         <form action="CriarPrato.php" method="POST" id="form_prato">
             <label> <br>Nome do Prato
-                    <input type="text" name="nome_prato" required>
+                <input type="text" name="nome_prato" required>
+            </label>
+            <?php if (isset($name_error)) { ?>
+                <span><?php echo $name_error; ?></span>
+            <?php } ?>
+            <br>
+            <label> <br>Restaurante
+                <select name="restaurante">
+                    <?php
+                    $query2 = "SELECT nome FROM restaurante";
+                    $result2 = pg_query($connection, $query2);
+                    if (pg_affected_rows($result2) > 0) {
+                        for ($i = 0; $i < pg_affected_rows($result2); $i++) {
+                            $arr = pg_fetch_array($result2);
+                            ?>
+                            <option value="rest"><?php echo $arr['nome']; ?></option> //NÃO ESTÁ A FUNCIONAR
+                        <?php }
+                    } else { ?>
+                        <a href="CriarRestaurante.php">Criar Restaurante</a>
+                    <?php } ?>
+                </select>
             </label>
             <br>
             <label> <br>Tipo de prato
@@ -36,14 +74,19 @@ include('phpCriarPrato.php');
             </label>
             <br>
             <label> <br>Preço
-                <input type="number" name="preço_prato" required></label>
+                <input type="number" name="preco_prato" required></label>
             <br>
             <label> <br>Descrição
-                <input type="text" name="descrição_prato" required>
+                <input type="text" name="descricao_prato" required>
             </label>
             <br>
             <input type="submit" name="register" value="Guardar">
         </form>
+        <?php
+
+    }
+    pg_close($connection);
+    ?>
 </main>
 </body>
 </html>
--- CriarRestaurante.php
+++ CriarRestaurante.php
@@ -1,5 +1,26 @@
 <?php
-include('phpCriarRestaurante.php');
+//include('phpCriarRestaurante.php');
+session_start();
+include_once "acess_bd.php";
+if (isset($_POST['register_restaurante'])) {
+    $nome_restaurante = $_POST['nome_restaurante'];
+    $localizacao_restaurante = $_POST['localizacao_restaurante'];
+    $administrador_usergeral_username = $_SESSION['username'];
+
+    $query1 = "SELECT * FROM restaurante WHERE ('$nome_restaurante' = nome)";
+    $result1 = pg_query($connection, $query1);
+    //caso não exista, insere registo na BD
+    if (pg_affected_rows($result1) > 0) {
+        $name_error = "Já foi criado um restaurante com esse nome";
+    } else {
+        //insere dados inseridos pelo administrador na tabela prato
+        $query = "INSERT INTO restaurante (nome,localizacao, administrador_usergeral_username) VALUES ('$nome_restaurante', '$localizacao_restaurante','$administrador_usergeral_username')";
+        $result1 = pg_query($connection, $query);
+        //echo "SAVED";
+        //exit();
+        header('location: Homepage_Administrador.php');
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -18,9 +39,10 @@ include('phpCriarRestaurante.php');
 <main>
     <div>
         <p> CRIAR RESTAURANTE</p>
-        <?php if(isset($name_error)){?>
-            <span><?php echo $name_error;?></span>
-        <?php }?>
+        <?php if (($_SESSION['success']) && isset($_SESSION['success'])) {
+        if (isset($name_error)) { ?>
+            <span><?php echo $name_error; ?></span>
+        <?php } ?>
         <form action="CriarRestaurante.php" method="POST" id="form_restaurante">
             <label> <br>Nome
                 <input type="text" name="nome_restaurante" required>
@@ -33,6 +55,12 @@ include('phpCriarRestaurante.php');
             <input type="submit" name="register_restaurante" value="Guardar">
         </form>
     </div>
+    <?php
+    }
+
+    pg_close($connection);
+
+    ?>
 </main>
 </body>
 </html>
