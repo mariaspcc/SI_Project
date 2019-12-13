6e9c6c51e477aa862286a9803a3e4cd6607f4338
mariaspcc <43615324+mariaspcc@users.noreply.github.com>
2019-12-13 21:12:38 +0000

    codigo comentado

--- Encomenda_Pendente.php
+++ Encomenda_Pendente.php
@@ -25,52 +25,71 @@ session_start();
     if (isset($_SESSION['success']) && $_SESSION['success']) {
         $cliente_usergeral_username = $_SESSION['username'];
 
-        //trás a variavel da página Homepage_cliente e verifica se a variável existe
+        //variável da página Homepage_cliente e verifica se esta existe
         if (isset($_GET["variavel"])) {
+            //declaração/atribuição da variável
             $id = $_GET["variavel"];
+            //seleciona atributos da tabela prato onde a variavel id é igual ao id do prato selecionado
             $query2 = "SELECT id, nome, restaurante_nome, preco FROM prato WHERE id = '$id'";
             $result2 = pg_query($connection, $query2);
 
-            //vai inserir encomenda - determina o id maximo da encomenda (a ultima encomenda)
+            //ciclo que lê as linhas do $result2 (percorre tabela prato)
             while ($res = pg_fetch_array($result2)) {
+                //determina a ultima encomenda feita (id maior que existe na tabela encomenda)
                 $query3 = "SELECT max(id) FROM encomenda";
+                //resultado vai ser apenas um parametro da tabela
                 $result3 = pg_query($connection, $query3);
-                //verifica se a encomenda existe
+                //vai buscar esse valor com o index(0,0)
                 $valor = pg_fetch_result($result3, 0, 0);
 
-
+                //$_SESSION guarda o numero da encomenda em curso
+                //se este for diferente ao id da ultima encomenda ou se ainda não existir encomendas feitas
                 if (($_SESSION['encomenda_id'] <> $valor) || ($_SESSION['encomenda_id'] === 0)) {
+                    //atribuimos uma variavel que soma ao id maximo ($valor) + 1
+                    // (ex: nao existe ainda nenhuma 0+1= 1º encomenda || ultima encomenda feita tem id=3 cria nova com id = 4)
                     $id_enc = $valor + 1;
+                    //insere na tabela encomenda o id da encomenda e o username do cliente
                     $query4 = "INSERT INTO encomenda(id,cliente_usergeral_username) VALUES ($id_enc,'$cliente_usergeral_username')";
                     $result4 = pg_query($connection, $query4);
+                    //guarda o id da encomenda que acabou de ser criada
                     $_SESSION['encomenda_id'] = $id_enc;
                 }
 
+                //id da encomenda atual
                 $id_encomenda = $_SESSION['encomenda_id'];
+                //insere na tabela detalhe a quantidade do prato, o id do prato e o id da respetiva encomenda
                 $query5 = "INSERT INTO detalhe (quantidade, prato_id, encomenda_id) VALUES (1,'$id','$id_encomenda')";
                 $result5 = pg_query($connection, $query5);
 
             }
         }
+        //id da encomenda atual
         $id_encomenda = $_SESSION['encomenda_id'];
+        //buscar dados relativos ao prato inseridos na encomenda
+        //verifica se o id do prato na tabela prato é igual ao prato.id da tabela detalhe
+        // e se a encomenda_id da tabela detalhe é igual à variavel id_encomenda
         $query6 = "SELECT nome, preco, id FROM  prato, detalhe WHERE prato.id = detalhe.prato_id AND detalhe.encomenda_id = $id_encomenda";
         $result6 = pg_query($connection, $query6);
 
+        //ciclo for para escrever os dados dos pratos
         for ($i = 0; $i < pg_affected_rows($result6); $i++) {
             $arrayDetalhe = pg_fetch_array($result6);
             //$apagar = $i;
             ?>
+            <!--escreve o nome do prato selecionado-->
             <h1>
                 <?php
                 echo pg_fetch_result($result6, $i, 0);
                 ?>
             </h1>
+            <!--escreve o preço do prato selecionado -->
             <h2>
                 <?php
                 echo pg_fetch_result($result6, $i, 1);
                 ?>
                 €
             </h2>
+            <!--variavel = ao id do prato selecionado -->
             <?php $id3 = pg_fetch_result($result6, $i, 2); ?>
             <label> <br>Quantidade
                 <input type="number" min="1" name="<?php echo $id3 ?>" required></label>
@@ -81,6 +100,7 @@ session_start();
                 <input type="submit" name="retirar_prato" value="Retirar prato da encomenda">
             </a>
             <?php
+            //variavel contem o id do prato (de uma encomenda especifica) para apagá-lo dessa encomenda
             if (isset($_GET["variavel2"])) {
                 $id2 = $_GET["variavel2"];
                 $query7 = "DELETE FROM detalhe WHERE encomenda_id = '$id_encomenda' AND prato_id = '$id2'";
