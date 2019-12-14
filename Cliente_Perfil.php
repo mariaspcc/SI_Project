de296a1c7fd493bf0b102b309ffb89dfd5c6e7bd
mariaspcc <43615324+mariaspcc@users.noreply.github.com>
2019-12-14 23:42:08 +0000

    yey

--- Cliente_Perfil.php
+++ Cliente_Perfil.php
@@ -23,17 +23,55 @@ session_start();
         $cliente_usergeral_username = $_SESSION['username'];
         ?>
         <a href="Homepage_Cliente.php"> Regressa à Homepage </a>
-
-        //SALDO ATUAL
-
-        //COMPRAS FEITAS
+        <br><br>
         <?php
-        $query = "select * from encomenda as E, detalhe AS D 
-    where E.cliente_usergeral_username='$cliente_usergeral_username' and E.id= D.encomenda_id 
-      and e.id= d.encomenda_id and d.prato_id= p.id
-    group by c. usergeral_username";
-        $result = pg_query($connection, $query);
+        $query2 = "select c.saldo-sum(d.quantidade*p.preco) 
+        from cliente AS C, encomenda as E, prato as P, detalhe AS D 
+        where c. usergeral_username= e.cliente_usergeral_username and e.id= d.encomenda_id and d.prato_id= p.id and e.terminada=TRUE
+        group by c. usergeral_username";
+        $result2 = pg_query($connection, $query2);
+        echo "Saldo atual: ".pg_fetch_result($result2, 0, 0);
+        ?>
+        <br><br>
+        <p> AS MINHAS ENCOMENDAS</p>
+        <?php
+        $query1 = "SELECT e.id, p.nome, d.quantidade, p.preco from cliente AS C, encomenda as E, prato as P, detalhe AS D
+        where c. usergeral_username= e.cliente_usergeral_username and e.id= d.encomenda_id and
+        d.prato_id= p.id and e.terminada=TRUE AND e.cliente_usergeral_username ='$cliente_usergeral_username'";
+        $result1 = pg_query($connection, $query1);
+        $total=0;
+        $soma2=0;
+        $aux1=0;
 
+        for ($i = 0; $i < pg_affected_rows($result1); $i++) {
+            //soma de cada linha
+            $soma1=pg_fetch_result($result1, $i, 2)*pg_fetch_result($result1, $i, 3);
+
+            //total=total+soma
+            $total+=$soma1;
+
+            //para mudar encomenda
+            if((pg_fetch_result($result1, $i, 0) <> $aux1) && ($aux1<>0)){
+
+                echo "&nbsp&nbsp&nbsp&nbsp"."Soma: ".$soma2;
+                ?><br><br><br><?php
+                $soma2=0;
+            }
+            $soma2+=$soma1;
+            $aux1=pg_fetch_result($result1, $i, 0);
+
+            echo "&nbsp&nbsp&nbsp&nbsp"."Encomenda: ".pg_fetch_result($result1, $i, 0);
+            echo "&nbsp&nbsp&nbsp&nbsp"."Nome do prato: ".pg_fetch_result($result1, $i, 1);
+            echo "&nbsp&nbsp&nbsp&nbsp"."Quantidade: ".pg_fetch_result($result1, $i, 2);
+            echo "&nbsp&nbsp&nbsp&nbsp"."Preço: ".pg_fetch_result($result1, $i, 3);
+            ?><br><?php
+
+            if($i===pg_affected_rows($result1)-1){
+                echo "&nbsp&nbsp&nbsp&nbsp"."Soma: ".$soma2;
+                ?><br><br><br><?php
+                echo "Total gasto em encomendas: ".$total;
+            }
+        }
     }
     ?>
 </main>
