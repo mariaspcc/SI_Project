80ec54b0835d8d012d2e0cb3cda92c535e6b2063
mariaspcc <43615324+mariaspcc@users.noreply.github.com>
2019-12-16 02:38:29 +0000

    update

--- DetalhePrato.php
+++ DetalhePrato.php
@@ -29,6 +29,44 @@ session_start();
             $query2 = "SELECT nome, tipo,restaurante_nome, descricao, preco FROM prato WHERE '$id'= id";
             $result2 = pg_query($connection, $query2);
 
+
+
+        //DESCONTOS
+        $today = date("Y-m-d H:i:s");
+        $desconto="select min(DI.desconto_id), D.restaurante_nome
+                        from desconto as D, desconto_info as DI 
+                        where DI.cliente_usergeral_username='$username'
+                        and D.id=DI.desconto_id and DI.usado=FALSE and D.duracao>'$today'
+                        group by D.restaurante_nome";
+        $result_desconto = pg_query($connection, $desconto);
+
+
+            $id_desconto = 0;
+            //vai ver se o prato tem desconto
+            for ($q = 0; $q < pg_affected_rows($result_desconto); $q++) {
+
+                $restaurante_tabela = pg_fetch_result($result_desconto, $q, 1);
+                $restaurante_prato = pg_fetch_result($result2, 0, 2);
+                if ($restaurante_tabela === $restaurante_prato) {
+                    $id_desconto = pg_fetch_result($result_desconto, 0, 0);
+                }
+            }
+            $result_valor_desc = 0;
+            $resultado_desconto = 0;
+            //se encontrar id desconto seleciona o valor do mesmo
+            if ($id_desconto > 0) {
+                $query_desc_rest = "select valor from desconto where desconto.id='$id_desconto'";
+                $result_valor_desc = pg_query($connection, $query_desc_rest);
+                $valor = pg_fetch_result($result_valor_desc, 0, 0);
+
+
+                $preco_prato = pg_fetch_result($result2, 0, 4);
+                $resultado_desconto = $preco_prato - ($valor / 100 * $preco_prato);
+
+            }
+            //FINAL DESCONTO
+
+
             for ($i = 0; $i < pg_affected_rows($result2); $i++) {
                 $arrayDetalhe = pg_fetch_array($result2);
                 $_SESSION['detalhe'] = $arrayDetalhe;
@@ -62,8 +100,11 @@ session_start();
                 ?>
                 €
             </h2>
+            <?php if ($id_desconto > 0) { ?>
+                <h5><?php echo "Com desconto fica: " . $resultado_desconto ?> €</h5>
+                <?php
+            }
 
-            <?php
         }
         $username = $_SESSION['username'];
         $administrador = "SELECT * FROM usergeral WHERE '$username' = username AND administrador = true";
